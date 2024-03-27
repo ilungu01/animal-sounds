723df2c13e0375d084d05a305aa9a2b9fe875793
IGOR L <igorl@directferries.com>
2024-03-27 22:57:04 +0200

    [AS-3] Iterate through list of animals

--- animal-sounds/Program.cs
+++ animal-sounds/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using animal_sounds;
 
-Console.WriteLine("Hello, World!");
+Cat cat = new Cat();
+cat.Name = "Vasile";
+cat.Sound = "Meow";
+
+Pig pig = new Pig("Piggy");
+pig.Sound = "Weee";
+
+List<IAnimal> animals = new List<IAnimal>
+{
+    cat,
+    pig,
+    new Dog("Doggo","Wuf"),
+    new Lion("Big boy", "Rawr")
+};
+
+foreach (var animal in animals)
+{
+    Console.WriteLine(animal.MakeSound());
+}
